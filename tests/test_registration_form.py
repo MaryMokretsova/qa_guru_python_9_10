5b21d6d3490919f64647363d3fd76fd2d60459be
MaryMokretsova <149939732+MaryMokretsova@users.noreply.github.com>
2023-12-22 00:07:02 +0300

    Update test_registration_form.py

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,32 +1,51 @@
-from qa_guru_python_9_10.pages.registration_page import RegistrationPage
+from pages.registration_page import RegistrationPage
 
 
 def test_registration():
     registration_page = RegistrationPage()
     registration_page.open_registration_page()
 
-def test_complete_todo():
-    browser.open('automation-practice-form')
-    browser.element('.practice-form-wrapper').should(have.text('Student Registration Form'))
+    # WHEN
+    registration_page.fill_first_name('Mariya')
+    registration_page.fill_last_name('Mokretsova')
+    registration_page.fill_email('cameron105@mail.ru')
+    registration_page.gender_selection()
+    registration_page.fill_user_mobile_number('9066507373')
 
-    browser.element('#firstName').should(be.blank).type('Mariya')
-    browser.element('#lastName').should(be.blank).type('Mokretsova')
-    browser.element('#userEmail').should(be.blank).type('cameron105@mail.ru')
-    browser.element('#gender-radio-2').double_click()
-    browser.element('#userNumber').should(be.blank).type('9066507373')
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__month-select').click().type('June').press_enter()
-    browser.element('.react-datepicker__year-select').click().type('1989').press_enter()
-    browser.element('.react-datepicker__day--027').click()
-    browser.element('#subjectsInput').should(be.blank).type('English').press_enter()
+    registration_page.fill_date_of_birth('1989', 'June', '27')
+    registration_page.fill_subject('English')
+
+
+    registration_page.fill_current_address('Sant-Peterburg, Aleksandra Matrosova')
+    registration_page.fill_state('Rajasthan')
+    registration_page.fill_city('Jaiselmer')
+    registration_page.click_submit()
+
+def test_complete_todo():
+    # browser.open('automation-practice-form')
+    # browser.element('.practice-form-wrapper').should(have.text('Student Registration Form'))
+
+    # browser.element('#firstName').should(be.blank).type('Mariya')
+    # browser.element('#lastName').should(be.blank).type('Mokretsova')
+    # browser.element('#userEmail').should(be.blank).type('cameron105@mail.ru')
+
+    # browser.element('#gender-radio-2').double_click()
+    #
+    # browser.element('#userNumber').should(be.blank).type('9066507373')
+    #
+    # browser.element('#dateOfBirthInput').click()
+    # browser.element('.react-datepicker__month-select').click().type('June').press_enter()
+    # browser.element('.react-datepicker__year-select').click().type('1989').press_enter()
+    # browser.element('.react-datepicker__day--027').click()
+    # browser.element('#subjectsInput').should(be.blank).type('English').press_enter()
     browser.element('[for=hobbies-checkbox-3]').perform(command.js.scroll_into_view).click()
     browser.element('#uploadPicture').send_keys(os.path.abspath(
         'pictures/run_girl.png'))
-    browser.element('#currentAddress').should(be.blank).type('Sant-Peterburg, Aleksandra Matrosova')
+    # browser.element('#currentAddress').should(be.blank).type('Sant-Peterburg, Aleksandra Matrosova')
 
-    browser.element('#react-select-3-input').type('Rajasthan').press_enter()
-    browser.element('#react-select-4-input').type('Jaiselmer').press_enter()
-    browser.element('#submit').press_enter()
+    # browser.element('#react-select-3-input').type('Rajasthan').press_enter()
+    # browser.element('#react-select-4-input').type('Jaiselmer').press_enter()
+    # browser.element('#submit').press_enter()
 
     browser.element('.modal-header').should(have.text('Thanks for submitting the form'))
     browser.all('.table').all('td')[1].should(have.exact_text('Mariya Mokretsova'))
