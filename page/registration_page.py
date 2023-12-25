78840797bbf04f8466ed154eb064d13d44d73380
Mary <cameron105@mail.ru>
2023-12-25 20:33:17 +0300

    high_branch

--- page/registration_page.py
+++ page/registration_page.py
@@ -1,56 +1,47 @@
-# import os
-#
-# from selene import browser, have, be, command
-#
-#
-# class RegistrationPage:
-#     def open_registration_page(self):
-#         browser.open('automation-practice-form')
-#         browser.element('.practice-form-wrapper').should(have.text('Student Registration Form'))
-#
-#     def fill_first_name(self, value):
-#         browser.element('#firstName').should(be.blank).type(value)
-#
-#     def fill_last_name(self, value):
-#         browser.element('#lastName').should(be.blank).type(value)
-#
-#     def fill_email(self, value):
+import os
+
+from selene import browser, have, be, command
+
+
+class RegistrationPage:
+    def open_registration_page(self):
+        browser.open('automation-practice-form')
+        browser.element('.practice-form-wrapper').should(have.text('Student Registration Form'))
+
+    def registration(self, user):
+        browser.element('#firstName').should(be.blank).type(user.first_name)
+        browser.element('#lastName').should(be.blank).type(user.last_name)
 #         browser.element('#userEmail').should(be.blank).type(value)
-#
-#     def gender_selection(self):
 #         browser.element('#gender-radio-2').double_click()
-#
-#     def fill_user_mobile_number(self, value):
 #         browser.element('#userNumber').should(be.blank).type(value)
-#
-#     def fill_date_of_birth(self, year, month, day):
 #         browser.element('#dateOfBirthInput').click()
 #         browser.element('.react-datepicker__month-select').click().type(month)
 #         browser.element('.react-datepicker__year-select').type(year)
 #         browser.element(f'.react-datepicker__day--0{day}').click()
-#
-#     def fill_subject(self, value):
 #         browser.element('#subjectsInput').should(be.blank).type(value).press_enter()
-#
-#     def fill_hobby_checkbox(self):
 #         browser.element('[for=hobbies-checkbox-3]').perform(command.js.scroll_into_view).click()
-#
-#     def fill_picture(self, value):
 #         browser.element('#uploadPicture').send_keys(os.path.abspath(
 #             'qa_guru_python_9_10/pictures/run_girl.png'))
-#
-#     def fill_current_adreess(self, value):
 #         browser.element('#currentAddress').should(be.blank).type(value)
-#
-#     def fill_state(self, value):
 #         browser.element('#react-select-3-input').type(value).press_enter()
-#
-#     def fill_city(self, value):
 #         browser.element('#react-select-4-input').type(value).press_enter()
-#
-#     def press_submit(self):
 #         browser.element('#submit').press_enter()
 #
+    first_name='Mariya',
+    last_name='Mokretsova',
+    user_email='cameron105@mail.ru',
+    user_gender='Famele',
+    user_phone_number='9066507373',
+    month='June',
+    year='1989',
+    day='11',
+    user_subject='English',
+    user_hobby='Music',
+    user_picture='run_girl.png',
+    user_current_address='Sant-Peterburg, Aleksandra Matrosova',
+    user_state='Rajasthan',
+    user_city='Jaiselmer',
+
 #     def should_registered_user_with(
 #         self,
 #         text,
@@ -79,3 +70,4 @@
 #
 #     def press_close(self):
 #         browser.element('#closeLargeModal').press_enter()
+
